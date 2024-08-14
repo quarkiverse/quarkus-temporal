d9eee67956557af11766d7a2a6dd1d502fb67e47
Loic Hermann <loichermann@Loics-MacBook-Pro.local>
2024-08-14 11:57:17 -0400

    fix package mixup in tests

--- extension/deployment/src/test/java/io/quarkiverse/temporal/deployment/config/NamedSimpleActivityImpl.java
+++ extension/deployment/src/test/java/io/quarkiverse/temporal/deployment/config/NamedSimpleActivityImpl.java
@@ -1,7 +1,6 @@
 package io.quarkiverse.temporal.deployment.config;
 
 import io.quarkiverse.temporal.ActivityImpl;
-import io.quarkiverse.temporal.deployment.discovery.SimpleActivity;
 
 @ActivityImpl(workers = "namedWorker")
 public class NamedSimpleActivityImpl implements SimpleActivity {
