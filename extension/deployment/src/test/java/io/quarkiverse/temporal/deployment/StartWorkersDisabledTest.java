a5e4ec9959dd3f80cac73473bbcda8857c1c92f4
Loic Hermann <loichermann@Loics-MacBook-Pro.local>
2024-08-29 16:47:48 -0400

    rework start worker tests

--- extension/deployment/src/test/java/io/quarkiverse/temporal/deployment/StartWorkersDisabledTest.java
+++ extension/deployment/src/test/java/io/quarkiverse/temporal/deployment/StartWorkersDisabledTest.java
@@ -1,30 +1,24 @@
 package io.quarkiverse.temporal.deployment;
 
-import java.util.List;
-
 import jakarta.inject.Inject;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-import io.quarkus.maven.dependency.Dependency;
 import io.quarkus.test.QuarkusUnitTest;
 import io.temporal.worker.WorkerFactory;
 
-@Disabled
 class StartWorkersDisabledTest {
 
     @RegisterExtension
     static final QuarkusUnitTest unitTest = new QuarkusUnitTest()
-            .setForcedDependencies(List.of(Dependency.of("io.quarkiverse.temporal", "quarkus-temporal-test", "999-SNAPSHOT")))
             .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
                     .addAsResource(
-                            new StringAsset("quarkus.temporal.enable-mock: true\nquarkus.temporal.start-workers: false\n"),
+                            new StringAsset("quarkus.temporal.start-workers: false\n"),
                             "application.properties"));
 
     @Inject
