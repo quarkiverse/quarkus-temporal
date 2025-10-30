0bb625f625f73aa49c085beb1ceb4ad49ef68682
Marcos Alcantara <marcos.alcantara@flippa.com>
2025-10-30 13:41:05 +1100

    Migrate `WorkerFactoryBuildtimeConfig` from Kotlin to Java

--- extension/runtime/src/main/java/io/quarkiverse/temporal/config/WorkerFactoryBuildtimeConfig.java
+++ extension/runtime/src/main/java/io/quarkiverse/temporal/config/WorkerFactoryBuildtimeConfig.java
@@ -1,26 +1,25 @@
-package io.quarkiverse.temporal.config
+package io.quarkiverse.temporal.config;
 
-import io.quarkus.runtime.annotations.ConfigGroup
-import io.smallrye.config.WithDefault
-
+import io.quarkus.runtime.annotations.ConfigGroup;
+import io.smallrye.config.WithDefault;
 
 @ConfigGroup
-interface WorkerFactoryBuildtimeConfig {
+public interface WorkerFactoryBuildtimeConfig {
     /**
      * Use virtual threads for workflow execution. Default is false.
      */
     @WithDefault("false")
-    fun useVirtualWorkflowThreads(): Boolean
+    boolean useVirtualWorkflowThreads();
 
     /**
      * Maximum number of threads the worker factory can use to run workflows. Default is 600.
      */
     @WithDefault("600")
-    fun maxWorkflowThreadCount(): Int
+    int maxWorkflowThreadCount();
 
     /**
      * Number of workflow instances to keep in cache per worker factory. Default is 600.
      */
     @WithDefault("600")
-    fun workflowCacheSize(): Int
+    int workflowCacheSize();
 }
