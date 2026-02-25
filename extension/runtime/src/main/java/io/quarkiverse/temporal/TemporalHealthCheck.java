8e8bf477617c1b06511a6d5394a36d0eb60c0e94
scott.maass <scott.maass@radpartners.com>
2026-02-25 15:49:32 -0600

    Make Temporal health check work for both local Docker and Temporal Cloud

--- extension/runtime/src/main/java/io/quarkiverse/temporal/TemporalHealthCheck.java
+++ extension/runtime/src/main/java/io/quarkiverse/temporal/TemporalHealthCheck.java
@@ -1,55 +1,47 @@
 package io.quarkiverse.temporal;
 
+import io.temporal.api.workflowservice.v1.GetSystemInfoRequest;
+import io.temporal.api.workflowservice.v1.GetSystemInfoResponse;
+import io.temporal.serviceclient.WorkflowServiceStubs;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 import org.eclipse.microprofile.health.Readiness;
 
-import io.temporal.client.WorkflowClient;
-
-/**
- * A readiness health check class for monitoring the health of Temporal service.
- * This class implements the HealthCheck interface to provide a custom health check
- * for Temporal's workflow service.
- */
-@Readiness
 @ApplicationScoped
+@Readiness
 public class TemporalHealthCheck implements HealthCheck {
+    WorkflowServiceStubs workflowServiceStubs;
 
-    // Injecting the WorkflowClient to interact with Temporal services
-    @Inject
-    WorkflowClient workflowClient;
+    public TemporalHealthCheck(WorkflowServiceStubs workflowServiceStubs) {
+        this.workflowServiceStubs = workflowServiceStubs;
+    }
 
-    /**
-     * The health check logic to determine if the Temporal service is healthy and ready to handle requests.
-     *
-     * @return HealthCheckResponse object indicating the health status of the Temporal service.
-     */
     @Override
     public HealthCheckResponse call() {
         HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("Temporal");
-
-        // Get the target HTTP URL information from WorkflowClient
-        String target = workflowClient.getWorkflowServiceStubs().getOptions().getTarget();
+        String target = "Unknown";
 
         try {
-            // Perform the health check by calling Temporal's health check API
-            io.grpc.health.v1.HealthCheckResponse response = workflowClient.getWorkflowServiceStubs().healthCheck();
+            if (workflowServiceStubs != null) {
+                target = workflowServiceStubs.getOptions().getTarget();
+                GetSystemInfoResponse systemInfo = workflowServiceStubs
+                        .blockingStub()
+                        .getSystemInfo(GetSystemInfoRequest.newBuilder().build());
 
-            // Retrieve the status from the health check response
-            io.grpc.health.v1.HealthCheckResponse.ServingStatus status = response.getStatus();
-
-            // Update the HealthCheckResponseBuilder based on the status of the service
-            responseBuilder = status == io.grpc.health.v1.HealthCheckResponse.ServingStatus.SERVING
-                    ? responseBuilder.up().withData(target, "UP") // If the service is serving, mark as up
-                    : responseBuilder.down().withData(target, "DOWN"); // If the service is not serving, mark as down
+                return responseBuilder.up().withData(target, "UP").build();
+            }
+            return responseBuilder
+                    .down()
+                    .withData("error", "Stubs not initialized")
+                    .build();
         } catch (Exception e) {
-            responseBuilder.down().withData(target, "DOWN");
+            return responseBuilder
+                    .down()
+                    .withData(target, "DOWN")
+                    .withData("error", e.getMessage())
+                    .build();
         }
-
-        return responseBuilder.build();
     }
 }
