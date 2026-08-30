a8eff8faf7ee96391e75c4181e2890b9acc54b87
melloware <mellowaredev@gmail.com>
2026-08-30 09:19:13 -0400

    test: re-enable StartWorkersEnabledTest/StartWorkersDisabledTest
    Both pass cleanly now; whatever originally motivated @Disabled (no
    explanation in history) is no longer reproducible.

--- test-extension/deployment/src/test/java/io/quarkiverse/temporal/test/deployment/StartWorkersDisabledTest.java
+++ test-extension/deployment/src/test/java/io/quarkiverse/temporal/test/deployment/StartWorkersDisabledTest.java
@@ -6,14 +6,12 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.quarkus.test.QuarkusUnitTest;
 import io.temporal.worker.WorkerFactory;
 
-@Disabled
 class StartWorkersDisabledTest {
 
     @RegisterExtension
